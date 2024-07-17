c5dd61708a1949e587748db59a8fd259338196ac
Marlon Munoz <marlon@marlonshade.com>
2024-07-17 01:12:29 -0400

    almost complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+// var bestCustomer = "not bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+// Call the function to overwrite the bestCustomer
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'someone'; // Assign an initial value
+
+function changeLeastFavoriteCustomer() {
+    // Attempt to change the constant (won't work)
+    leastFavoriteCustomer = 'new person'; // This will throw an error
+}
